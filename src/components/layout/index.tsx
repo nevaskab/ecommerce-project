6ffcba7a5aea40cb47ba6f7e5007f739de683881
Andressa Martins <andressa.devsystem@gmail.com>
2026-04-22 17:53:43 -0300

    [fix](layout): structure adjust

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,13 +1,10 @@
 import { Header } from "../header";
 import { Footer } from "../footer";
 import { Outlet } from "react-router-dom";
-import { Banner } from "../banner";
-
 export function Layout() {
   return (
     <>
       <Header />
-      <Banner />
       <Outlet />
       <Footer />
     </>
