2ae4b40f2341df31fa89706cdf4343e1826d9ef3
Andressa Martins <andressa.devsystem@gmail.com>
2026-05-14 12:34:12 -0300

    [add](icon-button): add link to props

--- src/components/icon-button/index.tsx
+++ src/components/icon-button/index.tsx
@@ -2,9 +2,17 @@ import type { ReactNode } from "react";
 
 type IconButtonProps = {
   className?: string;
+  link?: string;
   icon: ReactNode;
 };
 
 export default function IconButton(props: IconButtonProps) {
+  if (props.link) {
+    return (
+      <a href={props.link} id="icon-button">
+        {props.icon}
+      </a>
+    );
+  }
   return <button id="icon-button">{props.icon}</button>;
 }
