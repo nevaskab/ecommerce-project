ba277a3d2f6c315d3b39567cf9a31f338f441ab0
Andressa Martins <andressa.devsystem@gmail.com>
2026-05-14 12:38:40 -0300

    [feat](login): new page login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Cart } from "./pages/cart";
-
+import { Details } from "./pages/details";
 import { Layout } from "./components/layout";
+import { Login } from "./pages/login";
 
 const router = createBrowserRouter([
   {
@@ -13,11 +14,18 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/product/:id",
+        element: <Details />,
+      },
+      {
         path: "/cart",
         element: <Cart />,
       },
     ],
   },
 ]);
-
 export { router };
