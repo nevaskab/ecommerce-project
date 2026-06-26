7343c4f86ad43dfd5d9d9a14ef7468aefcaa9aba
Andressa Martins <andressa.devsystem@gmail.com>
2026-06-26 12:02:06 -0300

    [ci]: add link to frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://ecommerce-project-omega-hazel.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
@@ -95,7 +95,7 @@ app.post("/login", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
