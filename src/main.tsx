d33c6c9c4d289eeba80772a051e5171181951fb9
Andressa Martins <andressa.devsystem@gmail.com>
2026-08-31 16:40:28 -0300

    [feat]: wrap RouterProvider with ProductProvider for context management

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+import { ProductProvider } from "./contexts/APIContext";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ProductProvider>
+        <RouterProvider router={router} />
+      </ProductProvider>
     </AuthProvider>
   </StrictMode>,
 );
